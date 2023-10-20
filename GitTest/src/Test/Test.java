e83e818457946935a3a9de3b674a4479a37bce7d
powerpress0809 <powerpress0809@naver.com>
2023-10-20 12:19:34 +0900

    제발 좀 올라가라

---
+++ GitTest/src/Test/Test.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println(sum(10, 50000));
+		
+	}
+	
+	public static int sum (int x , int y) {
+		
+		return x + y;
+	}
+
+}
