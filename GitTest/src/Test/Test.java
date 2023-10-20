530e6cef55951613ad4692a9c821472cdd903f05
powerpress0809 <powerpress0809@naver.com>
2023-10-20 12:29:09 +0900

    클래스 나눠보기

--- GitTest/src/Test/Test.java
+++ GitTest/src/Test/Test.java
@@ -1,17 +1,11 @@
 package Test;
 
 public class Test {
-
 	public static void main(String[] args) {
 		
-		
-		System.out.println(sum(10, 50000));
+		Sum.sum(10,50);
 		
 	}
 	
-	public static int sum (int x , int y) {
-		
-		return x + y;
-	}
-
+	
 }
